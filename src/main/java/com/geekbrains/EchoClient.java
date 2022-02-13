eb706618b9c7c4e39c1ff23b379705d147926b8d
USER <sabanashvili.mihail@yandex.ru>
2022-02-13 22:22:43 +0300

    сделал отправку и прием без обработки ситуации с несколькими сообщениями

---
+++ src/main/java/com/geekbrains/EchoClient.java
@@ -0,0 +1,90 @@
+package com.geekbrains;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+    private final static String SERVER_ADDRESS = "localhost";
+    private final static int SERVER_PORT = 8080;
+    private static volatile boolean isExit = true;
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public EchoClient() {
+        try {
+            openConnection();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public void openConnection() throws IOException {
+        socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (isExit) {
+                        String messageFromServer = in.readUTF();
+                        if(messageFromServer.equals("/end")) {
+                            closeConnection();
+                            isExit = false;
+                            break;
+                        }
+                        System.out.println(messageFromServer);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (isExit) {
+                        Scanner scanner = new Scanner(System.in);
+                        String text = scanner.next();
+                        if(!isExit)
+                            break;
+                        sendMessage(text);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void closeConnection() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+            System.out.println("Клиент завершил работу");
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String text) {
+        try {
+            out.writeUTF(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        EchoClient echoClient = new EchoClient();
+    }
+}
---
+++ src/main/java/com/geekbrains/EchoServer.java
@@ -0,0 +1,65 @@
+package com.geekbrains;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer {
+    private static volatile boolean isExit = true;
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("Сервер запущен");
+            //Ожидаем клиента
+            Socket socket = serverSocket.accept();
+            System.out.println("Клиент подключился");
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (isExit) {
+                            String messageFromClient = in.readUTF();
+                            //я не очень понимаю, как должен реагировать сервер на отключение клиента. И на какой стороне нужно отключать клиента?
+                            if(messageFromClient.equals("/end")) {
+                                isExit = false;
+                                out.writeUTF("/end");
+                                break;
+                            }
+                            System.out.println(messageFromClient);
+                        }
+                        in.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (isExit) {
+                            Scanner scanner = new Scanner(System.in);
+                            String text = scanner.next();
+                            if(!isExit)
+                                break;
+                            out.writeUTF(text);
+                        }
+                        out.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
